2846386cc4c6a3420c34d16514fd15363a4025f9
steola <ishishi.steola@outlook.com>
2025-12-21 07:12:32 +0900

    update minecraft papar

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -10,12 +10,35 @@
     servers = {
       vanilla-plugin-server = {
         enable = true;
-        package = pkgs.vanillaServers.vanilla-1_21_1;
+        package = pkgs.paperServers.paper-1_21_1;
 
         serverProperties = {};
 	whitelist = {};
 	serverProperties = {
+	  # Port
           server-port = 25565;
+
+	  # game mode
+	  gamemode = "survival"; # survival / creative / adventure / spectator
+	  difficulty = "normal"; # peaceful / easy / normal / hard
+
+	  # chest
+	  enable-command-block = true;
+	  allow-flight = true;
+	  spawn-protection = 0;
+
+	  # multi
+	  online-mode = true;
+	  max-players = 10;
+
+	  # world
+	  level-name = "test world";
+	  view-distance = 10;
+	  simulation-distance = 10;
+	  
+	  # other
+	  pvp = true;
+	  motd = "NixOS Paper Server";
         };
       };
     };
