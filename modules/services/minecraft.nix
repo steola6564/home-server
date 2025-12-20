b42630ade8047fe7b23afd2ec67a3b5ee10641c9
steola <ishishi.steola@outlook.com>
2025-12-21 05:24:38 +0900

    enable minecraft-server

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -2,4 +2,18 @@
 {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ]; 
+
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+
+    servers = {
+      vanila-plugin-server = {
+        enable = true;
+
+	serverProperties = {};
+	whitelist = {};
+      };
+    };
+  };
 }
