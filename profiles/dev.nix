46f3bf3da9a5ea419a56be5da84a9ee648fa78fc
steola <ishishi.steola@outlook.com>
2025-12-21 04:25:10 +0900

    disable opentofu

--- profiles/dev.nix
+++ profiles/dev.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }: {
   programs.mtr.enable = true;
   environment.systemPackages = with pkgs; [
-    vim wget neovim git htop curl cloudflared-bin terraform opentofu
+    vim wget neovim git htop curl cloudflared-bin terraform
+    # opentofu
   ];
 }
 
