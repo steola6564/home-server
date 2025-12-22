fd75fe9e07f6b2a129fe88b9ca08b1ab895e5651
steola <ishishi.steola@outlook.com>
2025-12-22 20:43:41 +0900

    open port 433

--- modules/networking/common.nix
+++ modules/networking/common.nix
@@ -3,4 +3,8 @@
   networking.networkmanager.enable = true;
   # networking.firewall.allowedTCPPorts = [ 25565 ];
 
+  services.headscale.address = "0.0.0.0";
+  services.headscale.port = 443;
+
+  networking.firewall.allowedTCPPorts = [ 443 ];
 }
