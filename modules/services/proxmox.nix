0a27d4a9f72c264f85e61b4cac1e4392eb6a4b3b
steola <ishishi.steola@outlook.com>
2026-01-06 12:51:08 +0900

    add services/proxmox

---
+++ modules/services/proxmox.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.proxmox-ve = {
+    enable = true;
+
+    # Web UI が bind する IP
+    # LAN 内アクセス前提
+    ipAddress = "192.168.0.13";
+
+    # 初期は最小構成
+    openFirewall = true;
+
+    # bridge は後で定義（まず起動優先）
+    bridges = [ ];
+  };
+
+  # Proxmox Web UI
+  networking.firewall.allowedTCPPorts = [ 8006 ];
+}
+
