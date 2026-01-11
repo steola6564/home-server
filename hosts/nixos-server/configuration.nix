444670de77619dba0ad00a661f17fc1fc7e4d152
steola <ishishi.steola@outlook.com>
2026-01-11 23:55:59 +0900

    create directory hosts/nixos-server and copy hosts/*

---
+++ hosts/nixos-server/configuration.nix
@@ -0,0 +1,36 @@
+{ inputs, lib, pkgs, ... }: {
+  imports = [
+
+    # Hardware
+    ./hardware-configuration.nix
+
+    # Boot
+    ../modules/system/base.nix
+    ../modules/system/boot.nix
+
+    # Service
+    ../modules/services/common.nix
+    ../modules/services/ssh.nix
+    # ../modules/services/nextcloud.nix
+    ../modules/services/cloudflared.nix
+    # ../modules/services/minecraft.nix
+    # ../modules/services/headscale.nix
+    ../modules/networking/tailscale.nix
+    ../modules/services/proxmox.nix
+    ../modules/networking/bridge.nix
+
+    # User
+    ../modules/users/steola.nix
+    ../modules/users/cloudflared.nix
+
+    # ops
+    ../modules/pkgs.nix
+    ../modules/ops/agenix.nix
+    ../modules/ops/terraform.nix
+  ];
+
+  networking.hostName = "nixos-server";
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  system.stateVersion = "25.05";
+}
+
