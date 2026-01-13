3f1dbc1e2f16c0d62857de82e6330f48c1337f8c
steola <taiga.ishishi@gmail.com>
2026-01-14 00:54:48 +0900

    update configuration of import

--- hosts/minecraft/configuration.nix
+++ hosts/minecraft/configuration.nix
@@ -8,15 +8,14 @@
       ../../modules/system/boot.nix
       ../../modules/services/ssh.nix
       ../../modules/users/steola.nix
+      ../../modules/networking/common.nix
       ../../modules/pkgs.nix
+      ../../modules/home-manager.nix
+      ../../modules/services/minecraft.nix
     ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
   networking.hostName = "minecraft"; # Define your hostname.
-
-  networking.networkmanager.enable = true;
-
   system.stateVersion = "25.11"; # Did you read the comment?
 }
 
