670acb701c86025567b4572f44e1434c0c73a068
steola <taiga.ishishi@gmail.com>
2026-01-14 16:43:43 +0900

    allow firewall 25565

--- modules/networking/common.nix
+++ modules/networking/common.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   networking.networkmanager.enable = true;
-  # networking.firewall.allowedTCPPorts = [ 25565 ];
+  networking.firewall.allowedTCPPorts = [ 25565 ];
 
   # services.headscale.address = "0.0.0.0";
   # services.headscale.port = 443;
